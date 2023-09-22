99b1abce1b3151daa33cfd52762f9d2712afe76e
KarenCastilloBrito <143989290+KarenCastilloBrito@users.noreply.github.com>
2023-09-21 23:50:14 -0600

    pagoNSucesionAritmetica.R

--- pagoNSucesionAritmetica.R
+++ pagoNSucesionAritmetica.R
@@ -1,12 +1,11 @@
 pagoNSucesionAritmetica <- function(saldo, meses, incremento) {
-  # Desarrollamos la ecuación mencionada anteriormente para encontrar x1
-  x1 <- (saldo / (meses / 2) - (meses - 1) * incremento) / 2
-  
   # Calculamos xn para n = meses
-  xn <- x1 + (meses - 1) * incremento
-  return(xn)
+  pago_n <- saldo + (meses - 1) * incremento
+  return(pago_n)
 }
-
-# Invocar la función con los valores directamente
-cat("El monto de la mensualidad en el mes 72 es:", sprintf("$%.2f", pagoNSucesionAritmetica(25000.00, 72, 50)))
-
+saldo=25000
+meses=72
+incremento=50
+respuesta=pagoNSucesionAritmetica(saldo, meses, incremento)
+# Imprimir el resultado
+cat("El monto de la mensualidad en el mes 72 es:", respuesta, "\n")
